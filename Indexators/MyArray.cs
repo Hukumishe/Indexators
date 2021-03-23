ba14b383a4c882660a542e49cf171eaa9c0fdd64
Никита Федоров <nikfedorov65@gmail.com>
2021-03-23 15:39:43 +0600

    Добавьте файлы проекта.

---
+++ Indexators/MyArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class MyArray : IEnumerable
+    {
+        int[] arr;
+
+        public MyArray(int size)
+        {
+            arr = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+
+            set
+            {
+                arr[index] = value * value;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Indexators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            MyArray arr = new MyArray(5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int check;
+                arr[i] = check =  rnd.Next(1,10);
+                Console.WriteLine("Исходное число: " + check);
+                Console.WriteLine("Число в массиве: " + arr[i]);
+            }
+        }
+    }
+}
